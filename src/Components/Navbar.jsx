6a494e9ed7155d72ebfddf0d2932d81e6ac14cea
Anne Arhipoff <anmaar28421@gmail.com>
2024-02-24 19:33:58 +0200

    Navbar fixes, new font to site

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,24 +7,27 @@ import Projects from './Projects';
 
 function Navbar (){
 
-    const [value, setValue] = useState("Matopeli");
+    const [value, setValue] = useState("Fribago");
   
     const handleChange = (event, newValue) => {
+      console.log(event.target.selected);
+      event.target.classList.add("box_shadow");
         setValue(newValue);
     };
   
       return (
-        // TABLIST KAPEAMMAKSI MOBIILIISSA; SITTEN MOBINÄKYMÄT!!
+        <>
         <TabContext value={value}>
           <TabList className="navLinks" value={value} onChange={handleChange} aria-label="my projects choosing tabs">
-            <Tab label="Matopeli" value="Matopeli" />
-            <Tab label="Onnenpyörä" value="Onnenpyora" />
-            <Tab label="Fribago" value="Fribago" />
-            <Tab label="IPA Konevuokraamo" value="IpaKonevuokraamo" />
-            <Tab label="Kirjanpitäjä" value="Kirjanpitaja" />
-            <Tab label="Tämä portfolio" value="Portfolio" />
+            <Tab label="Matopeli" value="Matopeli" style={{ fontSize: '1.2em', fontWeight: 600 }}/>
+            <Tab label="Onnenpyörä" value="Onnenpyora" style={{ fontSize: '1.2em', fontWeight: 600 }}/>
+            <Tab label="Fribago" value="Fribago" style={{ fontSize: '1.2em', fontWeight: 600 }}/>
           </TabList>
-
+          <TabList className="navLinks2" value={value} onChange={handleChange} aria-label="second projects choosing tabs">
+            <Tab label="IPA Konevuokraamo" value="IpaKonevuokraamo" style={{ fontSize: '1.2em', fontWeight: 600 }}/>
+            <Tab label="Kirjanpitäjä" value="Kirjanpitaja" style={{ fontSize: '1.2em', fontWeight: 600 }}/>
+            <Tab label="Portfolio" value="Portfolio" style={{ fontSize: '1.2em', fontWeight: 600 }}/>
+          </TabList>
           <TabPanel value="Matopeli"><Projects projectName={value} /></TabPanel>
           <TabPanel value="Onnenpyora"><Projects projectName={value} /></TabPanel>
           <TabPanel value="Fribago"><Projects projectName={value} /></TabPanel>
@@ -32,6 +35,7 @@ function Navbar (){
           <TabPanel value="Kirjanpitaja"><Projects projectName={value} /></TabPanel>
           <TabPanel value="Portfolio"><Projects projectName={value} /></TabPanel>
         </TabContext>
+        </>
       );
 }
 
