e35892b799ec3482de9f3393cb24154cfa50d3a4
Anne Arhipoff <anmaar28421@gmail.com>
2024-03-01 20:13:30 +0200

    Minor changes
    Add of styles to Navbar Tabs, fribago news image added, Konevuokraamo heading removed, Header text removed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -11,7 +11,6 @@ function Navbar (){
   
     const handleChange = (event, newValue) => {
       console.log(event.target.selected);
-      event.target.classList.add("box_shadow");
         setValue(newValue);
     };
   
@@ -19,15 +18,18 @@ function Navbar (){
         <>
         <TabContext value={value}>
           <TabList className="navLinks" value={value} onChange={handleChange} aria-label="my projects choosing tabs">
-            <Tab label="Matopeli" value="Matopeli" style={{ fontSize: '1.2em', fontWeight: 600 }}/>
-            <Tab label="Onnenpyörä" value="Onnenpyora" style={{ fontSize: '1.2em', fontWeight: 600 }}/>
-            <Tab label="Fribago" value="Fribago" style={{ fontSize: '1.2em', fontWeight: 600 }}/>
+            <Tab label="Fribago" icon={ <img src="img/Fribago/fribago_logo.ico" />} iconPosition='end' value="Fribago" style={{ fontSize: '1.5em', fontWeight: 600, fontFamily: '"Baloo Paaji 2"', color: '#7cffd5', padding: 0 }}/>
+            <Tab label="Matopeli" value="Matopeli" style={{ fontWeight: 600, fontFamily: 'Honk', letterSpacing: '2px' }}/>
           </TabList>
-          <TabList className="navLinks2" value={value} onChange={handleChange} aria-label="second projects choosing tabs">
-            <Tab label="IPA Konevuokraamo" value="IpaKonevuokraamo" style={{ fontSize: '1.2em', fontWeight: 600 }}/>
-            <Tab label="Kirjanpitäjä" value="Kirjanpitaja" style={{ fontSize: '1.2em', fontWeight: 600 }}/>
-            <Tab label="Portfolio" value="Portfolio" style={{ fontSize: '1.2em', fontWeight: 600 }}/>
+          <TabList className="navLinks2" value={value} onChange={handleChange} aria-label="third projects choosing tabs">
+            <Tab icon={ <img src="img/Kirjanpitaja/kirjanpitaja_logo.png" height="45" backgroundColor="#98B252"/>}  value="Kirjanpitaja"/>
+            <Tab label="Onnenpyörä" className='multicolorText' value="Onnenpyora" style={{ fontWeight: 600 }}/>
           </TabList>
+          <TabList className="navLinks3" value={value} onChange={handleChange} aria-label="third projects choosing tabs">
+            <Tab label="IPA Konevuokraamo" value="IpaKonevuokraamo" style={{ fontWeight: 600, color: "#fb9d10", textShadow: "1px 1px 2px black", fontFamily: "Quantico" }}/>
+            <Tab label="Portfolio" value="Portfolio" style={{ fontWeight: 600, color: "var(--lessBrightYellow" }}/>
+          </TabList>
+          <hr></hr>
           <TabPanel value="Matopeli"><Projects projectName={value} /></TabPanel>
           <TabPanel value="Onnenpyora"><Projects projectName={value} /></TabPanel>
           <TabPanel value="Fribago"><Projects projectName={value} /></TabPanel>
