57c2c4c5656866e0ff0061f7f522c8855553c6e2
Anne Arhipoff <anmaar28421@gmail.com>
2024-03-04 19:04:57 +0200

    Responsivity changes to moreAboutMe-page

--- src/Components/Future.jsx
+++ src/Components/Future.jsx
@@ -4,8 +4,8 @@ function Future (){
 
     return (
         <>
-        <p>Nyt keväällä 2024, aloitan ahkeran harjoittelu-/työpaikan hakuprosessin. Toivottavaksi siksi sinäkin olet tänne eksynyt &#128521;.</p>
-        <p>Tässä hakuprosessin ja oman vakkarityön ohessa alkukevään jatkuu Javascript, Backend ja Saavutettavuus-kurssit. Keväämmällä, kuorman hieman kevennyttyä, on aika panostaa Fribagoon, jotta saadaan kesäksi harrastajille lisää toimintoja. Siinä sivussa jos vielä opettelisi jotain uutta tai tekisi omia projekteja. Oikeastaan toivottavasti niin päin, että harkkapaikka varmistuu ennen sitä ja voin jo valmiiksi harjoitella uuden työnantajan tarvitsemia taitoja!</p>
+        <p className="smallerText">Nyt keväällä 2024, aloitan ahkeran harjoittelu-/työpaikan hakuprosessin. Toivottavaksi siksi sinäkin olet tänne eksynyt &#128521;.</p>
+        <p className="smallerText">Tässä hakuprosessin ja oman vakkarityön ohessa alkukevään jatkuu Javascript, Backend ja Saavutettavuus-kurssit. Keväämmällä, kuorman hieman kevennyttyä, on aika panostaa Fribagoon, jotta saadaan kesäksi harrastajille lisää toimintoja. Siinä sivussa jos vielä opettelisi jotain uutta tai tekisi omia projekteja. Oikeastaan toivottavasti niin päin, että harkkapaikka varmistuu ennen sitä ja voin jo valmiiksi harjoitella uuden työnantajan tarvitsemia taitoja!</p>
         </>);
 }
 
