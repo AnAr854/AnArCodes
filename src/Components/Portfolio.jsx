511c5ca0b7c6527bd655868a430c28282f59e042
Anne Arhipoff <anmaar28421@gmail.com>
2024-03-11 11:08:40 +0200

    Update Portfolio.jsx

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-function Portfolio (){
-    return(
+function Portfolio() {
+    return (
         <>
-            <p>Tämän sivuston tarkoituksena on näyttää muutamia projektejani koulusta ja sen ulkopuolelta, sekä palautella mieleen Udemyssä käymääni Fullstack-kurssia ajalta ennen opiskelua alalle. Sivusto on toteutettu Reactilla, apuna käyttäen Material UI-kirjastoa. Kuten ulkoasusta huomaa, ei design-puoli ole ominta alaa, mutta pääosassa onkin aiemmat projektit ja portfolion mahdollistaminen työnhaussa.</p>
+            <p>Tämän sivuston tarkoituksena on näyttää muutamia projektejani koulusta ja sen ulkopuolelta, sekä palautella mieleen Udemyssä käymääni Fullstack-kurssia ajalta ennen opiskelua alalle. Sivusto on toteutettu Reactilla, apuna käyttäen Material UI-kirjastoa.</p>
             <p>Sivua päivitän sitä mukaa, kun tulee uusia projekteja tai ideoita sivun toiminnallisuuksiin tai ulkoasuun.</p>
             <p>Tämän portfolion repositorio löytyy täältä : <a className="githubLink" href="https://github.com/AnAr854/AnArCodes" target="_blank" rel="noreferrer">Githubiin</a></p>
         </>
