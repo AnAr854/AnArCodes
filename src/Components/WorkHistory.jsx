e5529725c21e1be3c66d5846b7260eb464981f4e
Anne Arhipoff <anmaar28421@gmail.com>
2024-03-12 12:32:44 +0200

    Update WorkHistory.jsx

--- src/Components/WorkHistory.jsx
+++ src/Components/WorkHistory.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import AcUnitIcon from '@mui/icons-material/AcUnit';
 
-function WorkHistory (){
-  return(
+function WorkHistory() {
+  return (
     <>
-    <ul>
-        <li><AcUnitIcon style={{marginRight: "0.5em", color: "#ffdf22"}}/>Kemet Electronics Oy, Vaihetyöntekijä/varatiiminvetäjä, 2006 -</li>
-        <li><AcUnitIcon style={{marginRight: "0.5em", color: "#ffdf22"}}/>Autogrilli Rasti Juntunen, kesätyö/vkonlopputyö, 2004-2005</li>
-        <li><AcUnitIcon style={{marginRight: "0.5em", color: "#ffdf22"}}/>Hotelli Ivalo, Karaoke-emäntä, 3kk, 2003</li>
-        <li><AcUnitIcon style={{marginRight: "0.5em", color: "#ffdf22"}}/>Ämmän Leipä, Kiireapulainen, 2002</li>
-        <li><AcUnitIcon style={{marginRight: "0.5em", color: "#ffdf22"}}/>Mannerheimin Lastensuojeluliitto, Lastenhoitaja, 1999-2002</li>
-        <li><AcUnitIcon style={{marginRight: "0.5em", color: "#ffdf22"}}/>Kerttuojan Lomamökit, Leirintäalueen hoitaja, kesätyö, 1998-2001</li>
-    </ul>
-    <p>Työhistoriani ei ehkei suoraan ole relevanttia tulevalle ammatilleni, mutta kaikki työpaikat ovat osaltaan antaneet paljon hyvää harjoitusta asiakaspalvelusta ja tiimityöskentelystä, sekä kehittäneet uuden oppimisen taitojani. Historiani paljastaa myös sen, että olen aina ollut työtä pelkäämätön ja ahkera.</p>
+      <ul>
+        <li><AcUnitIcon style={{ marginRight: "0.5em", color: "#ffdf22" }} />Kemet Electronics Oy, Vaihetyöntekijä/varatiiminvetäjä, 2006 -</li>
+        <li><AcUnitIcon style={{ marginRight: "0.5em", color: "#ffdf22" }} />Autogrilli Rasti Juntunen, kesätyö/vkonlopputyö, 2004-2005</li>
+        <li><AcUnitIcon style={{ marginRight: "0.5em", color: "#ffdf22" }} />Hotelli Ivalo, Karaoke-emäntä, 3kk, 2003</li>
+        <li><AcUnitIcon style={{ marginRight: "0.5em", color: "#ffdf22" }} />Ämmän Leipä, Kiireapulainen, 2002</li>
+        <li><AcUnitIcon style={{ marginRight: "0.5em", color: "#ffdf22" }} />Mannerheimin Lastensuojeluliitto, Lastenhoitaja, 1999-2002</li>
+        <li><AcUnitIcon style={{ marginRight: "0.5em", color: "#ffdf22" }} />Kerttuojan Lomamökit, Leirintäalueen hoitaja, kesätyö, 1998-2001</li>
+      </ul>
+      <p>Kaikki työpaikat ovat osaltaan antaneet paljon hyvää harjoitusta asiakaspalvelusta ja tiimityöskentelystä, sekä kehittäneet uuden oppimisen taitojani. Historiani paljastaa myös sen, että olen aina ollut työtä pelkäämätön ja ahkera.</p>
     </>
   );
 }
