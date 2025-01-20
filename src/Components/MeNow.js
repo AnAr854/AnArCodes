268fa38fec4e9c6019dcbc10bc758c21a4ff81e0
AnAr854 <83339747+AnAr854@users.noreply.github.com>
2025-01-20 17:22:02 +0200

    Update on MeNow and typofix in WorkHistory

--- src/Components/MeNow.js
+++ src/Components/MeNow.js
@@ -4,9 +4,10 @@ function MeNow() {
     return (
         <>
             <p>Kipinä koodaamiseen syttyi sattumalta vuonna 2019, ja siitä lähtien palo on vain yltynyt. Itsensä haastaminen, uuden opetteleminen ja ongelmien ratkominen on juuri tällaiselle jatkuvia haasteita kaipaavalle tarkoitettua. Koodaaminen on välillä hermoja raastavaa, mutta se tunne joka tulee onnistumisesta, on todellakin sen arvoinen!</p>
-            <p>Tällä hetkellä opiskelen tietojenkäsittelyä Hämeen ammattikorkeakoulussa. Yleisiä opintoja on kasassa marraskuussa 2024 143 opintopistettä, joten opinnot teorian osalta alkaa olla loppusuoralla. Vuoden 2024 loppuun mennessä loputkin 15op kyberturvallisuutta on suoritettuna. Parhaillaan etsinkin kuumeisesti harjoittelu- tai työpaikkaa alkavaksi mahdollisimman pian. Myös opinnäytetyöehdotuksia otetaan vastaan. Yhteyden minuun saa LinkedInin kautta ja linkin löydät sivun alaosasta. </p>
+            <p>Tällä hetkellä opiskelen tietojenkäsittelyä Hämeen ammattikorkeakoulussa. Teorian osalta kaikki on suoritettuna. Parhaillaan etsinkin kuumeisesti harjoittelu- tai työpaikkaa alkavaksi mahdollisimman pian, myös opinnäytetyöehdotuksia otetaan vastaan.</p>
             <p>Työskentelen parhaillaan täysipäiväisesti opintojen ohessa teollisuudessa kondensaattorien valmistuksessa ja tarkoituksena on suorittaa alanvaihto kohti unelma-ammattiani koodaajana mahdollisimman pian.</p>
-            <p>Päivitetty 26.10.2024</p>
+            <p>Yhteyden minuun saa LinkedInin kautta ja linkin löydät sivun alaosasta. </p>
+            <p>Päivitetty 20.01.2025</p>
         </>
     );
 }
--- src/Components/WorkHistory.js
+++ src/Components/WorkHistory.js
@@ -5,7 +5,7 @@ function WorkHistory() {
   return (
     <>
       <ul>
-        <li><AcUnitIcon style={{ marginRight: "0.5em", color: "#ffdf22" }} />Kemet Electronics Oy, Vaihetyöntekijä/varatiiminvetäjä, 2006 -</li>
+        <li><AcUnitIcon style={{ marginRight: "0.5em", color: "#ffdf22" }} />Kemet Electronics Oy, Vaihetyöntekijä/Tiiminvetäjä, 2006 -</li>
         <li><AcUnitIcon style={{ marginRight: "0.5em", color: "#ffdf22" }} />Autogrilli Rasti Juntunen, kesätyö/vkonlopputyö, 2004-2005</li>
         <li><AcUnitIcon style={{ marginRight: "0.5em", color: "#ffdf22" }} />Hotelli Ivalo, Karaoke-emäntä, 3kk, 2003</li>
         <li><AcUnitIcon style={{ marginRight: "0.5em", color: "#ffdf22" }} />Ämmän Leipä, Kiireapulainen, 2002</li>
